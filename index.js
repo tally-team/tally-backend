a68ca927f5b1b6695e63f17b02425dad8f97aebc
Ellie Ly <elliel.4797@gmail.com>
2022-03-04 18:35:47 -0500

    inital boilerplate

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const PORT = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 
 const init = async () =>{
+    try{
+        //await mongoose.connect(`mongodb://localhost:${PORT}/test`)
+        app.listen(PORT,()=> console.log(`Listening on port ${PORT}`))
+    } catch(e){
+        console.log('errorrrrr')
+    }
+};
 
-}
+init();
