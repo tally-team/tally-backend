b42599d459f50b6cea5a14e051c1b11f5d4dfbd4
Ellie Ly <elliel.4797@gmail.com>
2022-03-06 21:53:49 -0500

    removed 'main' attribute from package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const PORT = process.env.PORT || 8080;
-const connectDB = require('./mongoose')
+const connectDB = require('./db')
 const mongoose = require('mongoose');
 
 const init = async () =>{
