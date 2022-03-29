6cd57646efaa481e0faee3963fc102a6de48701c
Ellie Ly <elliel.4797@gmail.com>
2022-03-29 19:34:01 -0400

    Feat: Tests for User GET route

---
+++ test/user.test.data.js
@@ -0,0 +1,19 @@
+const userSeed = [
+    {
+        uuid: '1',
+        userName: 'testA',
+        password: 'testPassA'
+    },
+    {
+        uuid: '2',
+        userName: 'testB',
+        password: 'testPassB'
+    },
+    {
+        uuid: '3',
+        userName: 'testC',
+        password: 'testPassC'
+    }
+]
+
+module.exports = userSeed;
---
+++ test/user.test.js
@@ -0,0 +1,45 @@
+const { connectDatabase, disconnectDatabase } = require("../utils/test-utils/db.util");
+
+const supertest = require('supertest')
+const app = require('../app');
+const request = supertest(app);
+
+const User = require('../db/schema/user');
+const userTestData = require('./user.test.data');
+
+describe('user routes', () =>{
+    beforeAll(() => {
+        connectDatabase();
+    });
+
+    beforeEach(async ()=>{
+        for(const userData of userTestData){
+            const newUser = new User({
+                uuid: userData.uuid,
+                userName: userData.userName,
+                password: userData.password
+            })
+            await newUser.save()
+        }
+    })
+    
+    afterEach(async ()=>{
+        await User.deleteMany()
+    })
+
+    afterAll(() => {
+        disconnectDatabase();
+    });
+    describe('GET route', () =>{
+        it('should get all users from database', async () =>{
+            const res = await request.get('/api/users');
+            //console.log(res)
+            expect(res.status).toBe(200)
+            expect(res.body.length).toBe(userTestData.length)
+            expect(res.body[0].uuid).toBe(userTestData[0].uuid)
+            expect(res.body[0].userName).toBe(userTestData[0].userName)
+            expect(res.body[0].password).toBe(userTestData[0].password)
+        })
+
+    })
+})
