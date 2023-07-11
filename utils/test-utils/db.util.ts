9fe734d141cf95e44a4f5410aa8d82f7f4f09ece
Ellie <elliel.4797@gmail.com>
2023-07-11 10:48:06 -0400

    [task-52] test-utils/ conversion to Typescript (#56)
    * convert to TS and removed mongoose options because they've changed
    
    * adjusted mongo declaration and uses

--- utils/test-utils/db.util.ts
+++ utils/test-utils/db.util.ts
@@ -1,19 +1,14 @@
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
+import mongoose from "mongoose";
+import {MongoMemoryServer} from 'mongodb-memory-server'
 
-let mongo = new MongoMemoryServer;
+let mongo: MongoMemoryServer | null = null;
 
 const connectDatabase = async () => {
   try {
     mongo = await MongoMemoryServer.create();
     const uri = mongo.getUri();
 
-    const mongooseOpts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    };
-
-    await mongoose.connect(uri, mongooseOpts);
+    await mongoose.connect(uri);
   } catch(error) {
     throw(error);
   }
@@ -23,8 +18,10 @@ const disconnectDatabase = async () => {
   try {
     await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
-    await mongo.stop();
-    mongo = null;
+    if(mongo){
+      await mongo.stop();
+      mongo = null;
+    }
   } catch(error) {
     throw(error);
   }
