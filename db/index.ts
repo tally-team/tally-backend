08a9eac466715213f94bfad289f9c781adec43c0
Joanne <jl7105@nyu.edu>
2023-07-22 23:36:35 -0400

    testing eslint and prettier again

--- db/index.ts
+++ db/index.ts
@@ -2,6 +2,12 @@ import mongoose from 'mongoose';
 
 import config from '../config';
 
+const noTypeVariable = 'testing testing';
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
 const connectDB = async (mode: string) => {
   try {
     await mongoose.connect(config.db[mode], {});
