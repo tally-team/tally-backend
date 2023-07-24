e696811fef5673cb7fef5e242d8c1a0629de008d
loannejee <95735163+loannejee@users.noreply.github.com>
2023-07-24 19:49:30 -0400

    Loannejee/#44 set up eslint and prettier (#62)
    * #60 fixed typescript eslint/prettier errors and regression testing
    
    * testing eslint
    
    * testing eslint
    
    * testing!
    
    * testing eslint and prettier again
    
    * #44-set-up-eslint-and-prettier
    
    * testing once more
    
    * last commit for #44 ticket
    
    * no-unused-vars set to error

--- index.ts
+++ index.ts
@@ -3,19 +3,19 @@ import connectDB from './db';
 import config from './config';
 import mongoose from 'mongoose';
 
-const init = () =>{
-    const MODE = app.settings.env
-    const PORT = config.port[MODE]
+const init = () => {
+  const MODE = app.settings.env;
+  const PORT = config.port[MODE];
 
-    try{
-        connectDB(MODE);
-        mongoose.connection.once('open', () =>{
-            console.log("Connected to mongoDB");
-            app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
-        })
-    } catch(e){
-        throw(e);
-    }
+  try {
+    connectDB(MODE);
+    mongoose.connection.once('open', () => {
+      console.log('Connected to mongoDB');
+      app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    });
+  } catch (e) {
+    throw e;
+  }
 };
 
 init();
